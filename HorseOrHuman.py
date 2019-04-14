108904c2062e00aba163e18536b92ef0cb0f8563
Pranav-Dinesh <49243926+Pranav-Dinesh@users.noreply.github.com>
2019-04-14 15:58:07 +0530

    Create HorseOrHuman.py

---
+++ HorseOrHuman.py
@@ -0,0 +1,154 @@
+!wget --no-check-certificate \
+    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/horse-or-human.zip \
+    -O /tmp/horse-or-human.zip
+    
+import os
+import zipfile
+
+#The contents of the .zip are extracted to the base directory /tmp/horse-or-human, which in turn each contain horses and humans subdirectories.
+local_zip = '/tmp/horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/horse-or-human')
+zip_ref.close()
+
+# Directory with our training horse pictures
+train_horse_dir = os.path.join('/tmp/horse-or-human/horses')
+
+# Directory with our training human pictures
+train_human_dir = os.path.join('/tmp/horse-or-human/humans')
+
+#To see how the filenames look like
+train_horse_names = os.listdir(train_horse_dir)
+print(train_horse_names[:10])
+
+train_human_names = os.listdir(train_human_dir)
+print(train_human_names[:10])
+
+#To print the total number of files:
+print('total training horse images:', len(os.listdir(train_horse_dir)))
+print('total training human images:', len(os.listdir(train_human_dir)))
+
+#Now let's take a look at a few pictures to get a better sense of what they look like. First, configure the matplot parameters:
+%matplotlib inline
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Parameters for our graph; we'll output images in a 4x4 configuration
+nrows = 4
+ncols = 4
+
+# Index for iterating over images
+pic_index = 0
+
+# Set up matplotlib fig, and size it to fit 4x4 pics
+fig = plt.gcf()
+fig.set_size_inches(ncols * 4, nrows * 4)
+
+pic_index += 8
+next_horse_pix = [os.path.join(train_horse_dir, fname) 
+                for fname in train_horse_names[pic_index-8:pic_index]]
+next_human_pix = [os.path.join(train_human_dir, fname) 
+                for fname in train_human_names[pic_index-8:pic_index]]
+
+for i, img_path in enumerate(next_horse_pix+next_human_pix):
+  # Set up subplot; subplot indices start at 1
+  sp = plt.subplot(nrows, ncols, i + 1)
+  sp.axis('Off') # Don't show axes (or gridlines)
+
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+
+plt.show()
+
+import tensorflow as tf
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
+#Let's set up data generators that will read pictures in our source folders, convert them to float32 tensors, 
+#and feed them (with their labels) to our network. 
+#We'll have one generator for the training images and one for the validation images. 
+#Our generators will yield batches of images of size 300x300 and their labels (binary).
+
+#As you may already know, data that goes into neural networks should usually be normalized in some way to 
+#make it more amenable to processing by the network. (It is uncommon to feed raw pixels into a convnet.) 
+#In our case, we will preprocess our images by normalizing the pixel values to be in the [0, 1] range 
+#(originally all values are in the [0, 255] range).
+
+#In Keras this can be done via the keras.preprocessing.image.ImageDataGenerator class using the rescale parameter. 
+#This ImageDataGenerator class allows you to instantiate generators of augmented image batches (and their labels) 
+#via .flow(data, labels) or .flow_from_directory(directory). 
+#These generators can then be used with the Keras model methods that accept data generators as inputs: 
+#fit_generator, evaluate_generator, and predict_generator.
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1/255)
+
+# Flow training images in batches of 128 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        '/tmp/horse-or-human/',  # This is the source directory for training images
+        target_size=(300, 300),  # All images will be resized to 150x150
+        batch_size=128,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+        
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=8,  
+      epochs=15,
+      verbose=1)
+           
+import numpy as np
+from google.colab import files
+from keras.preprocessing import image
+
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+ 
+  # predicting images
+  path = '/content/' + fn
+  img = image.load_img(path, target_size=(300, 300))
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+
+  images = np.vstack([x])
+  classes = model.predict(images, batch_size=10)
+  print(classes[0])
+  if classes[0]>0.5:
+    print(fn + " is a human")
+  else:
+    print(fn + " is a horse")
+    
+import os, signal
+os.kill(os.getpid(), signal.SIGKILL)
